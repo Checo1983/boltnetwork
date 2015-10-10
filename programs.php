9eb17ccc8d9b34617053e1d03f1e99b063f28c0e
Checo <Jose Checo>
2015-10-11 00:21:13 +0200

    Modify programs.php. Add format date JS

--- programs.php
+++ programs.php
@@ -1,5 +1,11 @@
 <?php 
 
+	function getDateTimestamp($date) {
+		$str_date = $date;
+		$obj_date = DateTime::createFromFormat('D, d/m/Y - H:i', $str_date);
+		return $obj_date->getTimestamp();
+	}
+
 	$programs = array(
 		array(
 		 "series" => "Sherlock", 
@@ -8,7 +14,7 @@
 		  "season" => 3, 
 		  "episode" => "The Empty Hearse",
 		  "description" => "Two years after his reported Reichenbach Fall demise, Sherlock, who has been cleared of all fraud charges against him, returns with Mycroft's	help to a London under threat of terrorist attack. John has moved on and has a girlfriend, Mary Morstan. Sherlock enlists Molly to assist him, but when John is kidnapped by unknown assailants and is rescued by Sherlock and Mary, John returns to help find the terrorists and an underground plot to blow up the Houses of Parliament during an all night sitting on Guy Fawkes Night.",
-		  "datetime" => new DateTime("2014-12-31 21:00:00")
+		  "datetime" => getDateTimestamp("Wed, 31/12/2014 - 21:00")
 		),
 		array(
 		 "series" => "Arrested Development",
@@ -17,7 +23,7 @@
 	     "season" => 2,
 	     "episode" => "Righteous Brothers",
 	     "description" => "The model home collapses. Tobias and Kitty head to Las Vegas together.",
-	     "datetime" => new DateTime("2014-12-31 23:00:00")
+	     "datetime" => getDateTimestamp("Wed, 31/12/2014 - 23:00")
 		)
 	);
 
